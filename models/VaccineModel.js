3bf5dbfecebaa9d386d3fef9b38b83f953045fa2
adityabhagat007 <adityabhagat707@gmail.com>
2022-04-09 22:29:20 +0530

    hotfix

--- models/VaccineModel.js
+++ models/VaccineModel.js
@@ -12,7 +12,6 @@ const VaccineSchema = new Schema(
     hospitalId: {
       type: Schema.Types.ObjectId,
       ref: "Hospital",
-      default:null,
     },
     isDeleted: {
       type: Boolean,
